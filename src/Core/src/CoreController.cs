ff0ac13fdc7c4503be03c7298e15e7601d6b3c50
Diablo285 <diablo_285@freemail.hu>
2015-04-22 16:48:14 +0200

    Continuing work on GameCore

--- src/Core/src/CoreController.cs
+++ src/Core/src/CoreController.cs
@@ -3,26 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Core.src.InterFaces;
+using System.Windows.Input;
 
+using Core.src.InterFaces;
+using Core.src.Utils;
+
+
+using System.Collections.ObjectModel;
 
 namespace Core.src
 {
-    class CoreController : ICore
+    class CoreController : BaseViewModel
     {
-        protected Int32 registeredGames;
-        protected List<IGameApp> games;
+        protected CoreModel model;
+
+        public DelegateCommand LaunchGameCommand
+        {
+            get;
+            protected set;
+        }
+
+        public ObservableCollection<IGameApp> GameList
+        {
+            get
+            {
+                return model.Games;
+            }
+        }
+
+        public Int32 GameNum
+        {
+            get
+            {
+                return model.GameNum;
+            }
+        }
 
         public CoreController()
         {
-            registeredGames = 0;
-            games = new List<IGameApp>();
+            model = new CoreModel();
+
+            LaunchGameCommand = new DelegateCommand(param => { onGameLaunch(param); });
+
+            // Register Game applications
         }
 
-        public void register(IGameApp game)
+        private void onGameLaunch(object param)
         {
-            games.Add(game);
-            ++registeredGames;
+            Int32 Id = (Int32)param;
+            IGameApp current = GameList[(int)Id];
         }
     }
 }
--- src/Core/src/CoreModel.cs
+++ src/Core/src/CoreModel.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using Core.src.InterFaces;
 
 namespace Core.src
 {
-    class CoreModel
+    class CoreModel : ICore
     {
+        protected Int32 registeredGames;
+        protected ObservableCollection<IGameApp> games;
+        
+        public ObservableCollection<IGameApp> Games
+        {
+            get
+            {
+                return Games;
+            }
+        }
+
+        public Int32 GameNum
+        {
+            get
+            {
+                return registeredGames;
+            }
+        }
+
+        public CoreModel()
+        {
+            registeredGames = 0;
+            games = new ObservableCollection<IGameApp>();
+        }
+
+        public void register(IGameApp game)
+        {
+            ++registeredGames;
+            games.Add(game);
+        }
     }
 }
